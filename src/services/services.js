7b549f931c46877c427b536d30f740ccdb4b75e0
eva vargas <evargasabg@gmail.com>
2021-12-28 15:54:58 -0400

    bla

--- src/services/services.js
+++ src/services/services.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
 
-module.exports = {
-  getDataFromApi: async function () {
-    const result = await axios.get("https://reqres.in/api/users?per_page=20");
+export const getDataFromApi = async () => {
+  const result = await axios.get("https://reqres.in/api/users?per_page=20");
 
-    return result;
-  },
+  return result;
 };
+
+// module.exports = {
+//   getDataFromApi: async function () {
+//     const result = await axios.get("https://reqres.in/api/users?per_page=20");
+
+//     return result;
+//   },
+// };
